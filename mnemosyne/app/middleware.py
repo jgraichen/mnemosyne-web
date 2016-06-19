b94e69ee6bfd6f58abbe5d59b9ffad37bef02fa7
Jan Graichen <jg@altimos.de>
2016-06-19 13:12:42 +0200

    Fix db middleware
    Use single client created in async lock but multiple connections, one
    for each request.

--- mnemosyne/app/middleware.py
+++ mnemosyne/app/middleware.py
@@ -1,12 +1,27 @@
+import asyncio
 import aiopg
 
+_lock = asyncio.Lock()
+_db_pool = False
+
+async def _acquire_pool():
+    global _db_pool
+
+    if _db_pool:
+        return _db_pool
+
+    async with _lock:
+        if not _db_pool:
+            _db_pool = await aiopg.create_pool('dbname=mnemosyne_development', maxsize=50)
+
+    return _db_pool
+
+
 async def DatabaseMiddleware(app, handler):
-    db = await aiopg.create_pool('dbname=mnemosyne_development')
+    db = await _acquire_pool()
 
     async def middleware_handler(request):
         request.db = db
-
-        print(request)
 
         return await handler(request)
 
